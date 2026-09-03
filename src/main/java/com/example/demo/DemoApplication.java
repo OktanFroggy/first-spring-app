8c6dd97e53779fe70367ce391c686c4258549b7c
OktanFroggy <150390792+OktanFroggy@users.noreply.github.com>
2026-09-03 17:48:13 +0300

    обнова, сайт, стили

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,22 +2,20 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication
+@Controller
+
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-
-
 	}
 
-	@GetMapping("/hello")
-	public String sayhello() {
-		return "Нахуй иди";
+	@GetMapping("/")
+	public String showMainPage() {
+		return "index";
 	}
-
 }
